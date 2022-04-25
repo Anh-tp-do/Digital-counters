343349b76ff9f878f3aaace5407c0ab7c28dd1d3
Anh Do <36804143+Anh-tp-do@users.noreply.github.com>
2022-04-25 12:55:46 -0500

    Create Johnson counter.v

---
+++ Johnson counter.v
@@ -0,0 +1,15 @@
+// Resizable Johnson counter
+module johnson_counter(
+  input clk, rst; 
+  output reg [WIDTH-1:0] out
+);
+  always @(posedge clk)
+    begin
+      if (!rst)
+        out <= 1;
+      else
+        begin
+          out[WIDTH-1] ,+ ~out[0];
+        end 
+    end 
+endmodule 
