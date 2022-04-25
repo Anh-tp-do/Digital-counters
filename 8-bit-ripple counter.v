9f231f60bc0c1d21992afde7758ba9b996d179bc
Anh Do <36804143+Anh-tp-do@users.noreply.github.com>
2022-04-25 12:50:00 -0500

    Update 8-bit-ripple counter.v

--- 8-bit-ripple counter.v
+++ 8-bit-ripple counter.v
@@ -30,13 +30,13 @@ module ripple_counter(
   wire q1, q2, q3, q4, q5, q6, q7, q8;
   wire not_q1, not_q2, not_q3, not_q4,not_q5,not_q6,not_q7,not_q8,;
   // Instantiate dff module
-  dff dff1(.d[d], .clk[clk], .q[0], .rst[rst]); // 
-  dff dff2(.d, .clk[], .q[], .rst);
-  dff dff3(.d, .clk[], .q[], .rst);
-  dff dff4(.d, .clk[], .q[], .rst);
-  dff dff5(.d, .clk[], .q[], .rst);
-  dff dff6(.d, .clk[], .q[], .rst);
-  dff dff7(.d, .clk[], .q[], .rst);
-  dff dff8(.d, .clk[], .q[], .rst);
+  dff dff1(.d[not_q1], .clk[clk], .q[q1], .not_q[not_q1] .rst[rst]); // 1st FF gets the external clock signal
+  dff dff2(.d[not_q2], .clk[q1], .q[q2], .not_q[not_q2], .rst[rst]); // from 2nd FF, each FF gets clk signal from the previous FF
+  dff dff3(.d[not_q3], .clk[q2], .q[q3], not_q[not_q3], .rst[rst]);
+  dff dff4(.d[not_q4], .clk[q3], .q[q4], not_q[not_q4], .rs[rst]t);
+  dff dff5(.d[not_q5], .clk[q4], .q[q5], not_q[not_q5], .rst[rst]);
+  dff dff6(.d[not_q6], .clk[q5], .q[q6], not_q[not_q6], .rst[rst]);
+  dff dff7(.d[not_q7], .clk[q6], .q[q7], not_q[not_q7], .rst[rst]);
+  dff dff8(.d[not_q8], .clk[q7], .q[q8], not_q[not_q8], .rst[rst]);
  
 endmodule 
